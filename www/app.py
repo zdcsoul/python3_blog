a3e6ee373db6d9b8c325b121f4a8a122e649ab2e
zdcsoul <451914442@qq.com>
2018-11-19 17:39:38 +0800

    修改 web 骨架

--- www/app.py
+++ www/app.py
@@ -2,13 +2,20 @@
 # -*- coding: utf-8 -*-
 
 
+import asyncio
 from aiohttp import web
 
 
-def index(request):
+async def index(request):
     return web.Response(body=b'<h1>Welcome To My Blog</h1>', content_type='text/html')
 
 
-app = web.Application()
-app.add_routes([web.get('/', index)])
-web.run_app(app)
+async def main(loop):
+    server = web.Server(index)
+    await loop.create_server(server, "127.0.0.1", 8080)
+    print('server is started at http://127.0.0.1:8080')
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main(loop))
+loop.run_forever()
